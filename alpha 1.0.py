96f388361d4c7c11d03bb2adbfbb62ed7169fae9
meremees111 <tarvo.nippolainen@gmail.com>
2015-12-02 13:40:17 +0200

    Lisasin juurde funktsioonide paranduse ja latexi

--- alpha 1.0.py
+++ alpha 1.0.py
@@ -8,6 +8,13 @@ import os
 
 x, y, z, t = symbols('x y z t')
 
+def leia_nullkohad(sisend):
+    vastus = []
+    nullkohad = solve(sisend, x)
+    for i in nullkohad:
+        vastus.append([i,0])
+    return list(map(str, vastus))
+
 
 def lahenda_võrrand(sisend):
     return solve(sisend, x)
@@ -16,22 +23,40 @@ def võta_tuletis(sisend):
     return diff(sisend, x)
 
 def leia_miinimum(sisend):
-    nullkohad = solve(sisend, x)
     tuletis = diff(sisend, x)
-    minimum = []
-    for i in nullkohad:
-        if tuletis.replace(x, i) < 0:
-            minimum.append(i)
-    return map(str, minimum)
+    if diff(tuletis, x) != 0:
+        nullkohad = solve(tuletis)
+        minimum = []
+        for i in nullkohad:
+            if diff(tuletis, x) > 0:
+                minimum.append(i)
+        if len(minimum) > 0:
+            vastus = []
+            for i in minimum:
+                vastus.append([i, eval(sisend.replace('x', str(i)))])
+                return list(map(str, vastus))
+        else:
+            return ['Puudub']
+    else:
+        return ['Puudub']
 
 def leia_maksimum(sisend):
-    nullkohad = solve(sisend, x)
     tuletis = diff(sisend, x)
-    maksimum = []
-    for i in nullkohad:
-        if tuletis.replace(x, i) > 0:
-            maksimum.append(i)
-    return map(str, maksimum)
+    if diff(tuletis, x) != 0:
+        nullkohad = solve(tuletis)
+        maksimum = []
+        for i in nullkohad:
+            if diff(tuletis, x) < 0:
+                maksimum.append(i)
+        if len(maksimum) > 0:
+            vastus = []
+            for i in maksimum:
+                vastus.append([i, eval(sisend.replace('x', str(i)))])
+                return list(map(str, vastus))
+        else:
+            return ['Puudub']
+    else:
+        return ['Puudub']
 
 def leia_kasvamine(sisend):
     tuletis = diff(sisend, x)
@@ -78,6 +103,10 @@ def lisa_graafik(formula, xkohad):
     plt.plot(x, y)
     plt.axhline(0, color='black')
     plt.axvline(0, color='black')
+    init_printing(use_latex= 'mathjax')
+    x = symbols('x')
+    lateks = latex(eval(formula))
+    plt.title('$%s$' %lateks, fontsize = 25)
     plt.savefig('graafik.png')
     tahvel = Canvas(raam, height=600, width=800, background='white')
     img = PhotoImage(file="graafik.png")
@@ -92,9 +121,9 @@ def näita_tulemus():
             nullkohad_string = ttk.Label(raam, text='Nullkohad_BETA: ', font=('Cambria Math', 10, 'bold'))
             nullkohad_string.place(x=90, y=240)
 
-            nullkohad_vastus = ttk.Label(raam, text=[' , '.join(lahenda_võrrand(valem.get()))], font=('Cambria Math', 10, 'bold'))
+            nullkohad_vastus = ttk.Label(raam, text=[' , '.join(leia_nullkohad(valem.get()))], font=('Cambria Math', 10, 'bold'))
             nullkohad_vastus.place(x=250, y=240)
-
+            # Tegin ekstreemumite leidmist korda, peaks nüüd korrektselt töötama
             miinimum_string = ttk.Label(raam, text='Miinimum_BETA:', font=('Cambria Math', 10, 'bold'))
             miinimum_string.place(x=90, y=290)
 
@@ -107,42 +136,43 @@ def näita_tulemus():
             maksimum_vastus = ttk.Label(raam, text=[' , '.join(leia_maksimum(valem.get()))], font=('Cambria Math', 10, 'bold'))
             maksimum_vastus.place(x=250, y=340)
 
-            # Töötab hetkel ainult kui on paraboolne funktsioon, kus on ainult 1 vaheldumispunkt (Alpha v0.1)
-            kasvab_string = ttk.Label(raam, text='Kasvab_ALPHA:', font=('Cambria Math', 10, 'bold'))
-            kasvab_string.place(x=90, y=390)
+            # # Töötab hetkel ainult kui on paraboolne funktsioon, kus on ainult 1 vaheldumispunkt (Alpha v0.1)
+            # kasvab_string = ttk.Label(raam, text='Kasvab_ALPHA:', font=('Cambria Math', 10, 'bold'))
+            # kasvab_string.place(x=90, y=390)
+            #
+            # kasvab_vastus = ttk.Label(raam, text=[' , '.join(leia_kasvamine(valem.get()))], font=('Cambria Math', 10, 'bold'))
+            # kasvab_vastus.place(x=250, y=390)
+            # # Töötab hetkel ainult kui on paraboolne funktsioon, kus on ainult 1 vaheldumispunkt (Alpha v0.1)
+            # kahaneb_string = ttk.Label(raam, text='Kahaneb_ALPHA:', font=('Cambria Math', 10, 'bold'))
+            # kahaneb_string.place(x=90, y=440)
+            #
+            # kahaneb_vastus = ttk.Label(raam, text=[' , '.join(leia_kahanemine(valem.get()))], font=('Cambria Math', 10, 'bold'))
+            # kahaneb_vastus.place(x=250, y=440)
+            #
+            # # neid tuleb alles teha (Alpha v0.1)
+            # # Mingi hästi väike alpha nendest tehtud, et saavad mõnikord aru, kuid teine tuletis on lihtsalt arv (Alpha v0.1)
+            # kumerus_string = ttk.Label(raam, text='Kumerus_ALPHA:', font=('Cambria Math', 10, 'bold'))
+            # kumerus_string.place(x=90, y=490)
+            #
+            # kumerus_vastus = ttk.Label(raam, text=[' , '.join(leia_kumerus(valem.get()))], font=('Cambria Math', 10, 'bold'))
+            # kumerus_vastus.place(x=250, y=490)
+            #
+            # nõgusus_string = ttk.Label(raam, text='Nõgusus:_ALPHA', font=('Cambria Math', 10, 'bold'))
+            # nõgusus_string.place(x=90, y=540)
+            #
+            # nõgusus_vastus = ttk.Label(raam, text=[' , '.join(leia_nõgusus(valem.get()))], font=('Cambria Math', 10, 'bold'))
+            # nõgusus_vastus.place(x=250, y=540)
+            #
+            # # Seda saab varsti juba beta-ks nimetada :D (Alpha v0.1)
+            # käänupunkt_string = ttk.Label(raam, text='Käänupunkt_ALPHA:', font=('Cambria Math', 10, 'bold'))
+            # käänupunkt_string.place(x=90, y=590)
+            #
+            # käänupunkt_vastus = ttk.Label(raam, text=leia_käänupunkt(valem.get()), font=('Cambria Math', 10, 'bold'))
+            # käänupunkt_vastus.place(x=250, y=590)
 
-            kasvab_vastus = ttk.Label(raam, text=[' , '.join(leia_kasvamine(valem.get()))], font=('Cambria Math', 10, 'bold'))
-            kasvab_vastus.place(x=250, y=390)
-            # Töötab hetkel ainult kui on paraboolne funktsioon, kus on ainult 1 vaheldumispunkt (Alpha v0.1)
-            kahaneb_string = ttk.Label(raam, text='Kahaneb_ALPHA:', font=('Cambria Math', 10, 'bold'))
-            kahaneb_string.place(x=90, y=440)
 
-            kahaneb_vastus = ttk.Label(raam, text=[' , '.join(leia_kahanemine(valem.get()))], font=('Cambria Math', 10, 'bold'))
-            kahaneb_vastus.place(x=250, y=440)
+            lisa_graafik(valem.get(), range(-10, 10))
 
-            # neid tuleb alles teha (Alpha v0.1)
-            # Mingi hästi väike alpha nendest tehtud, et saavad mõnikord aru, kuid teine tuletis on lihtsalt arv (Alpha v0.1)
-            kumerus_string = ttk.Label(raam, text='Kumerus_ALPHA:', font=('Cambria Math', 10, 'bold'))
-            kumerus_string.place(x=90, y=490)
-
-            kumerus_vastus = ttk.Label(raam, text=[' , '.join(leia_kumerus(valem.get()))], font=('Cambria Math', 10, 'bold'))
-            kumerus_vastus.place(x=250, y=490)
-
-            nõgusus_string = ttk.Label(raam, text='Nõgusus:_ALPHA', font=('Cambria Math', 10, 'bold'))
-            nõgusus_string.place(x=90, y=540)
-
-            nõgusus_vastus = ttk.Label(raam, text=[' , '.join(leia_nõgusus(valem.get()))], font=('Cambria Math', 10, 'bold'))
-            nõgusus_vastus.place(x=250, y=540)
-
-            # Seda saab varsti juba beta-ks nimetada :D (Alpha v0.1)
-            käänupunkt_string = ttk.Label(raam, text='Käänupunkt_ALPHA:', font=('Cambria Math', 10, 'bold'))
-            käänupunkt_string.place(x=90, y=590)
-
-            käänupunkt_vastus = ttk.Label(raam, text=leia_käänupunkt(valem.get()), font=('Cambria Math', 10, 'bold'))
-            käänupunkt_vastus.place(x=250, y=590)
-
-            näita_graafik = ttk.Button(raam, text='Näita graafik!')
-            näita_graafik.place(x=125, y=650, height=60, width=140)
 
     else:
         tühi_sisend = ttk.Label(raam, text='Sisend on tühi.')
@@ -167,5 +197,5 @@ def on_closing():
         os.remove('graafik.png')
         raam.destroy()
 
-raam.protocol("WM_DELETE_WINDOW", on_closing)
+#raam.protocol("WM_DELETE_WINDOW", on_closing)
 raam.mainloop()
