bd89898ca9a042b96dbab91a2da17dd0c5704d3b
meremees111 <tarvo.nippolainen@gmail.com>
2015-12-04 09:41:07 +0200

    Lisasin vajalikud muutujad ja placeholderid

--- PlotMaster Basic Alpha v2.1.py
+++ PlotMaster Basic Alpha v2.1.py
@@ -290,18 +290,30 @@ def lisa_graafik(formula):
     nullkohad = latex(' , '.join(leia_nullkohad(formula)))
     miinimum = latex(' , '.join(leia_miinimum(formula)))
     maksimum = latex(' , '.join(leia_maksimum(formula)))
-    kasvab = latex(('(\infty, 0)(0, \infty)'))
+    kasvab = latex('(\infty, 0)(0, \infty)')
+    kahaneb = latex('\sqrt{20}')
+    kumerus = latex('PLACEHOLDER')
+    nõgusus = latex('PLACEHOLDER')
+    käänukoht = latex('PLACEHOLDER')
 
     plt.title('$%s$' % funktsioon, fontsize=25)
     data1 = ('Nullkohad: ' + '\n' + '$%s$' % nullkohad)
     data2 = ('Miinimumkohad: ' + '\n' +  '$%s$' % miinimum)
     data3 = ('Maksimumkohad: ' + '\n' + '$%s$' % maksimum)
-    data4 = ('Kasvamispiirkond:' + '\n' + '$%s$' % kasvab) #ei tööta
+    data4 = ('Kasvamispiirkond:' + '\n' + '$%s$' % kasvab)
+    data5 = ('Kahanemispiirkond:' + '\n' + '$%s$' % kahaneb)
+    data6 = ('Kumeruspiirkond:' + '\n' + '$%s$' % kumerus)
+    data7 = ('Nõgususpiirkond:' + '\n'+ '$%s$' % nõgusus)
+    data8 = ('Käänukohad:' + '\n'+ '$%s$' % käänukoht)
 
     pl.text(11, 8, data1)
     pl.text(11, 6, data2)
     pl.text(11, 4, data3)
     pl.text(11, 2, data4)
+    pl.text(11, 0, data5)
+    pl.text(11, -2, data6)
+    pl.text(11, -4, data7)
+    pl.text(11, -6, data8)
 
     pl.savefig('graafik.png', bbox_inches='tight')
 
@@ -381,6 +393,8 @@ def näita_tulemus():
 
         käänupunkt_vastus = ttk.Label(raam, text=leia_käänupunkt(valem.get()), font=('Cambria Math', 10, 'bold'))
         käänupunkt_vastus.place(x=250, y=590)'''
+
+
         lisa_graafik(valem.get())
 
     else:
@@ -389,7 +403,7 @@ def näita_tulemus():
 
 
 raam = Tk()
-raam.title('PLOTMASTER BASIC ALHPA 0.2.1')
+raam.title('PLOTMASTER BASIC ALPHA 0.3.0')
 kõrgus = 800
 laius = 1300
 raam.geometry(str(laius)+'x'+str(kõrgus))
